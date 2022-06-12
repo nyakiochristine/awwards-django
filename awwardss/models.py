e71872d271f2b5b50feec134bec89601ceb294cc
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-12 23:10:33 +0300

    ft:created profile class

--- awwardss/models.py
+++ awwardss/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+#profile,#post #rating
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    profile_picture = models.
+    
