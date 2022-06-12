db882abc6fafc38a5aef8e9bd1d7c5a9277c602b
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-12 23:21:39 +0300

    ft:created post model

--- awwardss/models.py
+++ awwardss/models.py
@@ -1,14 +1,30 @@
+from gzip import READ
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 import datetime as dt
 from cloudinary.models import CloudinaryField
-
 # Create your models here.
 #profile,#post #rating
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    profile_picture = models.
+    profile_picture = models.CloudinaryField()
+    bio= models.TextField(max_length=355,default='my bio',blank=True)
+    name = models.CharField(max_length=65,blank=True)
+    location = models.CharField(max_length=65,blank=True)
     
+    
+    
+    
+    
+    
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    url = models.URLField(max_length=200)
+    description = models.TextField(max_length=500)
+    technologies = models.CharField(max_length=100, blank=True)
+    photo = models.CloudinaryField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    date = models.DateTimeField(auto_now_add=True, blank=True)
