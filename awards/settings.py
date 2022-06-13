76bdbe23d0841a66d6e2790557f3eb2a1112bbed
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-13 03:11:34 +0300

    ft:added bootstrap

--- awards/settings.py
+++ awards/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'awwardss'
+    'bootstrap5',
+    'awwardss',
 ]
 
 MIDDLEWARE = [
