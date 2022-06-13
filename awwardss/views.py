4879a6e761b8667646c0e98a8adbc4dda69443f8
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-13 12:17:59 +0300

    ft:created the authentification view function

--- awwardss/views.py
+++ awwardss/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+import random
+from .forms import PostForm,SignupForm
+from .models import Profile,Rating,Post
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form= SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data.get('username')
+            raw_password= form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=raw_password)
+            login(request, user)
+            return redirect('index')
+        else:
+            form= SignupForm()
+        return render(request,'registration/signup.html',{'form':form})
