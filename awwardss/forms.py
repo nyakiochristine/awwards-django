f2564676e0071914caeb0667192416f4a0b1d7a0
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-20 17:34:44 +0300

    ft:created  image field for the post form

--- awwardss/forms.py
+++ awwardss/forms.py
@@ -1,9 +1,8 @@
-#from pyuploadcare.dj.models import ImageField
+from pyuploadcare.dj.models import ImageField
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Rating,Post,Profile
 from django.contrib.auth.models import User
-
 
 #1signup form for new users
 
@@ -17,13 +16,30 @@ class SignupForm(UserCreationForm):
 
 
 class PostForm(forms.ModelForm):
-    #photo= ImageField(label='')
+    photo= ImageField()
     photo = forms.FileField()
     
     
     class Meta:
         model = Post
         fields = ('photo', 'title', 'url', 'description', 'technologies')
+
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
+
+
 
 
 class RatingsForm(forms.ModelForm):
