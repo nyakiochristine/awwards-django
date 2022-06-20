6548e4e8e1abbe71024c5873ef0c8832e109f129
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-20 17:38:35 +0300

    ft:created url patterns for signup and rest framework

--- awwardss/urls.py
+++ awwardss/urls.py
@@ -1,8 +1,23 @@
-from django.urls import path,include
+from django.urls import include,path
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
+
+
 
 urlpatterns =[
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('api/', include(router.urls)),
     path('account/', include('django.contrib.auth.urls')),
+    path('<username>/profile', views.user_profile, name='userprofile'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('project/<post>', views.project, name='project'),
+    path('search/', views.search_project, name='search'),
 ]
